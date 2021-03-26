0140ea9484ba2eacb47494e424a19146b3b4a9b0
Jonas Grosse-Holz <aber@der-b.art>
2021-03-26 19:26:55 +0100

    test fetching shopping list

---
+++ shell.nix
@@ -0,0 +1,35 @@
+let
+  oxalica_overlay = import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ oxalica_overlay ]; };
+  unstable = import <nixos-unstable> {};
+  rust_channel = nixpkgs.rust-bin.stable."1.48.0".rust;
+in with nixpkgs;
+pkgs.mkShell {
+  buildInputs = [
+  ];
+
+  nativeBuildInputs = [
+    (rust_channel.override {
+      extensions = [ "rust-src" ];
+    })
+
+    # tools
+    git
+    clippy
+    llvmPackages.clang
+
+    unstable.cargo-audit
+    unstable.cargo-outdated
+  ];
+
+  # Useful backtraces
+  RUST_BACKTRACE = 1;
+
+  # compilation of -sys packages requires manually setting LIBCLANG_PATH
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+
+  # Workaround for https://github.com/mozilla/nixpkgs-mozilla/issues/240
+  # with Rust 1.46.0
+  LD_LIBRARY_PATH = ''${zlib.out}/lib''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}'';
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Amount {
+    value: f64,
+    unit: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SLItem {
+    id: String,
+    name: String,
+    amount: Option<Amount>,
+    category: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ShoppingList {
+    id: String,
+    title: String,
+    items: Vec<SLItem>,
+}
+
+fn main() -> Result<()> {
+    let body: String = ureq::get("http://list.tilman.ninja/api/Demo")
+        .call()?
+        .into_string()?;
+
+    let list: ShoppingList = serde_json::from_str(&body)?;
+    println!("{:#?}", list);
+    Ok(())
 }
