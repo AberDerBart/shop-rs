67273375d49b669f92d1d309f969a7777dd3bdea
puzzlewolf <mail@nora.pink>
2021-03-29 21:19:20 +0200

    wip cli

--- src/bin/shop.rs
+++ src/bin/shop.rs
@@ -1,15 +1,27 @@
 use structopt::StructOpt;
 use anyhow::Result;
-use shop_rs::SLItem;
-
 
 #[derive(StructOpt, Debug)]
 #[structopt(about = "interact with shopping lists")]
-enum Shop {
+struct Shop {
+    #[structopt(short, long)]
+    /// server where the shopping list is managed
+    server: Option<String>,
+    #[structopt(short, long)]
+    /// name of the shopping list
+    list: Option<String>,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
     Add {
+        /// the string representation of an item
         item: Vec<String>,
     },
     Remove {
+        /// the string representation of an item
         item: Vec<String>,
     },
 }
@@ -18,12 +30,12 @@ fn main() -> Result<()> {
     let opt = Shop::from_args();
     println!("{:?}", opt);
 
-    match opt {
-        Shop::Add{item: i} => {
+    match opt.cmd {
+        Command::Add{item: i} => {
             let i = i.join(" ");
             println!("add {}", i);
         },
-        Shop::Remove{item: i} => {
+        Command::Remove{item: i} => {
             let i = i.join(" ");
             println!("remove {}", i);
         },
