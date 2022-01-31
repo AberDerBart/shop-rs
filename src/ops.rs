5a0a259ae677c6c8e83a7a7386157b3250c4deeb
Jonas Grosse-Holz <aber@der-b.art>
2022-01-31 22:09:58 +0100

    print numbers when listing categories

--- src/ops.rs
+++ src/ops.rs
@@ -27,7 +27,12 @@ fn initial_sync(agent: &ureq::Agent, config: &Config) -> Result<State> {
     Ok(resp.into())
 }
 
-fn sync(agent: &ureq::Agent, config: &Config, state: State, include_categories: bool) -> Result<State> {
+fn sync(
+    agent: &ureq::Agent,
+    config: &Config,
+    state: State,
+    include_categories: bool,
+) -> Result<State> {
     let mut path = config.path();
     path.push_str("/sync");
 
@@ -117,14 +122,19 @@ pub fn print_list(config: &Config) -> Result<()> {
     Ok(())
 }
 
+fn print_categories_internal(categories: &Vec<CategoryDefinition>) {
+    let num_digits = format!("{}", categories.len()).len();
+    for (index, cat) in categories.iter().enumerate() {
+        println!("{:>n$}.{}", index + 1, cat.to_string_long(), n = num_digits);
+    }
+}
+
 pub fn print_categories(config: &Config) -> Result<()> {
     let agent = get_agent(config)?;
 
     let state = get_current_list(&agent, config)?;
-    for cat in &state.categories {
-        cat.println_long();
-    }
 
+    print_categories_internal(&state.categories);
     Ok(())
 }
 
@@ -133,24 +143,30 @@ fn random_color() -> String {
     let g = rand::random::<u8>();
     let b = rand::random::<u8>();
 
-    format!("#{:0>2x}{:0>2x}{:0>2x}", r,g,b)
+    format!("#{:0>2x}{:0>2x}{:0>2x}", r, g, b)
 }
 
 fn derive_category_short_name(name: &str) -> String {
     let short: String = name.chars().filter(|c| c.is_uppercase()).collect();
 
     if short.len() > 0 {
-        return short
+        return short;
     }
 
     if name.len() >= 3 {
-        return name[..3].to_uppercase()
+        return name[..3].to_uppercase();
     }
-    
+
     name.to_uppercase()
 }
 
-pub fn add_category(config: &Config, name: String, short_name: Option<String>, color: Option<String>, light_text: bool) -> Result<()> {
+pub fn add_category(
+    config: &Config,
+    name: String,
+    short_name: Option<String>,
+    color: Option<String>,
+    light_text: bool,
+) -> Result<()> {
     let agent = get_agent(config)?;
 
     let mut state = get_current_list(&agent, config)?;
@@ -158,17 +174,17 @@ pub fn add_category(config: &Config, name: String, short_name: Option<String>, c
     let short_name = short_name.unwrap_or_else(|| derive_category_short_name(&name));
     let color = color.unwrap_or_else(|| random_color());
 
-    state.categories.push(CategoryDefinition{
+    state.categories.push(CategoryDefinition {
         name,
         color,
         short_name,
         light_text,
-        id: Uuid::new_v4(),        
+        id: Uuid::new_v4(),
     });
 
     let state = sync(&agent, &config, state, true)?;
-    for cat in &state.categories {
-        cat.println_long();
-    }
+
+    print_categories_internal(&state.categories);
+
     Ok(())
 }
